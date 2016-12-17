92cc73e9e58c898a1ba91f5cafbc7c7017c5250e
aranga-au <aranga.nanayakkara@gmail.com>
2016-12-18 00:23:41 +1100

    refactor, fix  code to ignore path not in the acl

--- index.js
+++ index.js
@@ -1,167 +1,36 @@
-var jwt = require('jsonwebtoken');
+
 var _ = require('lodash');
 var q = require('q');
+var permissionValidateFactory = require('./lib/validate-permission');
+var authTokenFactory = require('./lib/auth-token-factory');
+var validateRequestCreator = require('./lib/validate-request');
 
 module.exports = function (secret, option, acl, permissionDef) {
+    var permissionValidator = permissionValidateFactory.create(secret,permissionDef,authTokenFactory);
+    var validateRequest = validateRequestCreator(acl);
 
     var authFactory = {
-        generateToken: generateToken,
-        verify: verify,
-        accessController: function(){
-			return accessController;
-		}
-		
-
+        generateToken: authTokenFactory.generateToken,
+        verify: authTokenFactory.verify,
+        accessController: function () {
+            return accessController;
+        }
     };
     return authFactory;
 
     //implementation
     function accessController(req, resp, next) {
 
-        if (!req.headers.authorization) {
-            resp.send({ status: "401", messsage: "cannot find authorization header" }, 401);
-			
-            return;
+        if (req.originalUrl) {
+            req.resource = req.originalUrl;
         }
-
-        var ary = _.split(req.headers.authorization, ' ');
-        if (ary.length != 2) {
-            resp.send({ status: 401, messsage: "Invalid authorization header",name:"Unauthorized access" }, 401);
-		
-            return;
-        }
-		if (req.originalUrl){
-			req.resource =   req.originalUrl;
-		}
-		
-        validate(ary[1],req).then(checkAccess).then(function (req) {
-            
+        validateRequest(req).then(permissionValidator).then(function(req){
             next();
-        }).catch(function (err) {
-            resp.send(err, 401);
-			return;
-			
-            
+        }).catch(function(err){
+            resp.send(err,err.status);
         });
 
-        function checkAccess(result) {
-            var deferred = q.defer();
-            var p = result.payload.loggedInAs;
-            var req = result.req;
-         
-            if (req.resource) {
-                
-                if (acl[req.resource] ){
-                    ary = acl[req.resource][req.method];
-                  
-                    if (_checkPermission(ary,p)){
-                        deferred.resolve(req);
-                    } 
-                    else{
-                        deferred.reject({status:401,message:"no access right for resource",name:"Unauthorized Access"});
-                    }   
-                }else{
-                    var eff=[];
-                    _.each(acl,function(v,k){
-                        if (_.endsWith(k,'*')){
-                            r = _.replace(k,'/*','');
-                           
-                            if (req.resource.indexOf(r) != -1){
-                                console.log('V',v[req.method]);
-                                if (v[req.method]){
-                                    eff =v[req.method];
-                                }
-                                
-                                
-                            }
-                        }    
-                    });
-                    if (eff.length ==0){
-                        deferred.reject({status:401,message:"no access right",name:"Unauthorized Access",err:new Error()});
-                    }
-                    else {
-                        var ret = _checkPermission(eff,p);
-                      
-                        if (ret){
-                             deferred.resolve(req);
-                        }else{
-                             deferred.reject({status:401,message:"no access right",name:"Unauthorized Access"});
-                        }
-
-                    }
-
-                }
-  
-            }
-            else {
-
-                deferred.reject({ status: 400, message: "invalid http request" });
-            }
-
-            //private methods
-            function _checkPermission(ary,p){
-				
-                var ret = false;
-                _.each(ary,function(v,i){
-		
-                    if (permissionDef[v] && (permissionDef[v] & p) > 0) {
-                      
-                       ret = true;
-                    }    
-                });
-                return ret;
-            }
-
-
-            return deferred.promise;
-
-
-
-        }
     }
 
 
-
-
-    function  generateToken(payload) {
-        var deferred = q.defer();
-        jwt.sign(payload, secret, option, function (err, token) {
-            //console.log(data);
-            if (err) {
-                deferred.reject(err);
-            }
-            else {
-                deferred.resolve(token);
-            }
-
-        });
-        return deferred.promise;
-    }
-    function verify(data) {
-        var deferred = q.defer();
-    
-        jwt.verify(data, secret, function (err, payload) {
-
-            if (err) {
-               
-                deferred.reject(err);
-                return;
-            }
-            //console.log(payload);
-            deferred.resolve(payload);
-        });
-
-        return deferred.promise;
-
-    }
-    function validate(token,req){
-        var deferred = q.defer();
-        verify(token).then(function(payload){
-            deferred.resolve({req:req,payload:payload});
-
-        }).catch(function(e){
-            deferred.reject(e);
-        });
-        return deferred.promise;
-    }
 };
